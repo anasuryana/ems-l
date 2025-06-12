5f2080b1ccd046d026451e73596ecda476e897f6
Ana Suryana <anasuryana10@gmail.com>
2025-06-12 19:33:37 +0700

    feat : chart api

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class DashboardController extends Controller
+{
+    function getChart1Data()
+    {
+        $data = [];
+        $dataDB = DB::table('tbl_pcb_logs')->where('status', '!=', 'Off')
+            ->where('date', date('Y-m-d'))
+            ->groupBy(DB::raw('HOUR(time)'))
+            ->select(
+                DB::raw("HOUR(TIME) time_"),
+                DB::raw("SUM(case when status = 'Red' then  qty end) ng"),
+                DB::raw("SUM(case when status = 'Yellow ' then  qty end) retry")
+            )->get();
+
+        if ($dataDB->count() == 0) {
+            $lastDate = DB::table('tbl_pcb_logs')->where('status', '!=', 'Off')
+                ->select(
+                    'date'
+                )->orderBy('date', 'desc')->first();
+
+            $dataDB = DB::table('tbl_pcb_logs')->where('status', '!=', 'Off')
+                ->where('date', $lastDate->date)
+                ->groupBy(DB::raw('HOUR(time)'))
+                ->select(
+                    DB::raw("HOUR(TIME) time_"),
+                    DB::raw("SUM(case when status = 'Red' then  qty end) ng"),
+                    DB::raw("SUM(case when status = 'Yellow ' then  qty end) retry")
+                )->get();
+        }
+
+
+        for ($i = 0; $i <= 24; $i++) {
+            $startHour = substr('0' . $i, -2);
+            $ng = $retry = 0;
+            foreach ($dataDB as $r) {
+                if (substr($startHour, 0, 2) == substr($r->time_, 0, 2)) {
+                    $ng = $r->ng ?? 0;
+                    $retry = $r->retry ?? 0;
+                    break;
+                }
+            }
+            $data[] = [
+                'time' =>  $startHour, //. '~' . substr('0' . ($i + 1), -2),
+                'ng' => $ng,
+                'retry' => $retry
+            ];
+        }
+
+        return ['data' => $data];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,4 +12,8 @@ Route::get('/user', function (Request $request) {
 
 Route::prefix('users')->group(function () {
     Route::post('login', [UserController::class, 'login']);
+});
+
+Route::prefix('report')->group(function () {
+    Route::get('chart1', [DashboardController::class, 'getChart1Data']);
 });
